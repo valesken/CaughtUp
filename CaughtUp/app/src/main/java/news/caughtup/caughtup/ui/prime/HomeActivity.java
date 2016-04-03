6c5b327dbe73466dae95b63636bbfb4eea47c17d
Dimitris Sarlis <sarlis.dim@gmail.com>
2016-04-03 11:39:25 -0700

    Added custom action bar in main app activity

--- CaughtUp/app/src/main/java/news/caughtup/caughtup/ui/prime/HomeActivity.java
+++ CaughtUp/app/src/main/java/news/caughtup/caughtup/ui/prime/HomeActivity.java
@@ -2,6 +2,11 @@ package news.caughtup.caughtup.ui.prime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import news.caughtup.caughtup.R;
 
@@ -11,5 +16,38 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.action_bar);
+        setSupportActionBar(myToolbar);
+        myToolbar.setNavigationIcon(R.drawable.profile_icon);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* User selected to go to their profile page, create the edit profile fragment */
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_items, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search_icon:
+                // User chose the "Search" item, show the search fragment
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
