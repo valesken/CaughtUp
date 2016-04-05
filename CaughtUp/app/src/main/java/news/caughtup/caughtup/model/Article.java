3f47a6e4e1cf8de1af39e30d6f427df343820596
Armin Mahmoudi <arminm89@gmail.com>
2016-04-04 21:00:24 -0700

    Added news feed

---
+++ CaughtUp/app/src/main/java/news/caughtup/caughtup/model/Article.java
@@ -0,0 +1,59 @@
+package news.caughtup.caughtup.model;
+
+import android.net.Uri;
+
+
+public class Article {
+    private String title;
+    private String date;
+    private int thumbnailID;
+    private String summary;
+    private Uri articleURI;
+
+    public Article(String title, String date, int thumbnailID, String summary, Uri articleURI) {
+        this.title = title;
+        this.date = date;
+        this.thumbnailID = thumbnailID;
+        this.summary = summary;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getThumbnailID() {
+        return thumbnailID;
+    }
+
+    public void setThumbnailID(int image) {
+        this.thumbnailID = image;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public Uri getArticleURI() {
+        return articleURI;
+    }
+
+    public void setArticleURI(Uri articleURI) {
+        this.articleURI = articleURI;
+    }
+}
