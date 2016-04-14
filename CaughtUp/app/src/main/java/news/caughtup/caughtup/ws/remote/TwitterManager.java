ec315340450e82552c33ce48a7ad89344558f461
Armin Mahmoudi <arminm89@gmail.com>
2016-04-13 19:52:12 -0700

    Added TwitterManager.java

---
+++ CaughtUp/app/src/main/java/news/caughtup/caughtup/ws/remote/TwitterManager.java
@@ -0,0 +1,15 @@
+package news.caughtup.caughtup.ws.remote;
+
+import news.caughtup.caughtup.entities.Article;
+
+public class TwitterManager implements ISocialMediaManager {
+    @Override
+    public void authenticate() {
+        //TODO
+    }
+
+    @Override
+    public void share(String message, Article article) {
+        //TODO
+    }
+}
