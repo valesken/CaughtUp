3e8e291b9efde25a826a225b31b49ebb1154d563
Armin Mahmoudi <arminm89@gmail.com>
2016-05-02 21:40:24 -0700

    Commented on util and remote packages

--- CaughtUp/app/src/main/java/news/caughtup/caughtup/ws/remote/IRest.java
+++ CaughtUp/app/src/main/java/news/caughtup/caughtup/ws/remote/IRest.java
@@ -1,5 +1,8 @@
 package news.caughtup.caughtup.ws.remote;
 
+/**
+ * Interface used to define HTTP REST operations to be implemented by REST clients.
+ */
 public interface IRest {
     void getCall(String endPoint, String jsonData, Callback callback);
     void postCall(String endPoint, String jsonData, Callback callback);
--- CaughtUp/app/src/main/java/news/caughtup/caughtup/ws/remote/ISocialMediaManager.java
+++ CaughtUp/app/src/main/java/news/caughtup/caughtup/ws/remote/ISocialMediaManager.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 
 import news.caughtup.caughtup.entities.Article;
 
+/**
+ * Interface to define Social Media Manager methods to share articles on social media.
+ */
 public interface ISocialMediaManager {
     void share(String message, Article article, Activity activity);
 }
